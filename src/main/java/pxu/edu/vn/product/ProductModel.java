7a0297fc9384351640c44f21f969cdcaddf06909
HungPhanzzz <hungphan290803@gmail.com>
2023-07-20 09:54:58 +0700

    đã hiển thị được sản phẩm o index.jsp

--- src/main/java/pxu/edu/vn/product/ProductModel.java
+++ src/main/java/pxu/edu/vn/product/ProductModel.java
@@ -1,67 +1,138 @@
 package pxu.edu.vn.product;
 
-import pxu.edu.vn.dao.DBConnection;
-import pxu.edu.vn.product.Product;
-
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
+import pxu.edu.vn.dao.DBConnection;
 
 public class ProductModel {
-    public List<Product> getAllProducts() {
-        List<Product> products = new ArrayList<>();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
+    public static ArrayList<Product> getAllProducts() throws Exception {
+        ArrayList<Product> productList = new ArrayList<>();
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
 
         try {
-            // Get the database connection
-            connection = DBConnection.getConnection();
-
-            // Prepare the SQL statement
+            conn = DBConnection.getConnection();
+            stmt = conn.createStatement();
             String sql = "SELECT * FROM products";
-            statement = connection.prepareStatement(sql);
-
-            // Execute the query
-            resultSet = statement.executeQuery();
-
-            // Iterate over the result set and create Product objects
-            while (resultSet.next()) {
-                int productID = resultSet.getInt("product_id");
-                String productName = resultSet.getString("product_name");
-                int categoryID = resultSet.getInt("category_id");
-                int brandID = resultSet.getInt("brand_id");
-                double price = resultSet.getDouble("price");
-                double discountedPrice = resultSet.getDouble("discounted_price");
-                String productImg = resultSet.getString("product_img");
-                String productInfo = resultSet.getString("product_info");
-
-                // Create a Product object and add it to the list
-                Product product = new Product(productID, productName, categoryID, brandID, price, discountedPrice, productImg, productInfo);
-                products.add(product);
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Product product = new Product();
+                product.setProductID(rs.getInt(1));
+                product.setProductName(rs.getString(2));
+                product.setCategoryID(rs.getInt(3));
+                product.setBrandID(rs.getInt(4));
+                product.setPrice(rs.getDouble(5));
+                product.setDiscountedPrice(rs.getDouble(6));
+                product.setProductImg(rs.getString(7));
+                product.setProductInfo(rs.getString(8));
+                productList.add(product);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            throw new Exception("Lỗi khi lấy dữ liệu từ cơ sở dữ liệu", e);
         } finally {
-            // Close the resources
             try {
-                if (resultSet != null) {
-                    resultSet.close();
+                if (rs != null) {
+                    rs.close();
                 }
-                if (statement != null) {
-                    statement.close();
+                if (stmt != null) {
+                    stmt.close();
                 }
-                if (connection != null) {
-                    connection.close();
+                if (conn != null) {
+                    conn.close();
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            } catch (SQLException ex) {
+                throw new Exception("Lỗi khi đóng kết nối", ex);
             }
         }
+        return productList;
+    }
 
-        return products;
+    // Hàm Thêm Dữ Liệu
+    public static void insertProduct(Product product) throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            conn = DBConnection.getConnection();
+            stmt = conn.createStatement();
+            String sql = "INSERT INTO products (product_name, category_id, brand_id, price, discounted_price, product_img, product_info) VALUES ('"
+                    + product.getProductName() + "', " + product.getCategoryID() + ", " + product.getBrandID() + ", "
+                    + product.getPrice() + ", " + product.getDiscountedPrice() + ", '" + product.getProductImg()
+                    + "', '" + product.getProductInfo() + "')";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new Exception("Lỗi khi thêm dữ liệu vào cơ sở dữ liệu", e);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new Exception("Lỗi khi đóng kết nối", ex);
+            }
+        }
+    }
+
+    // Hàm Sửa Dữ Liệu
+    public static void updateProduct(Product product) throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            conn = DBConnection.getConnection();
+            stmt = conn.createStatement();
+            String sql = "UPDATE products SET product_name = '" + product.getProductName() + "', category_id = "
+                    + product.getCategoryID() + ", brand_id = " + product.getBrandID() + ", price = "
+                    + product.getPrice() + ", discounted_price = " + product.getDiscountedPrice() + ", product_img = '"
+                    + product.getProductImg() + "', product_info = '" + product.getProductInfo() + "' WHERE product_id = "
+                    + product.getProductID();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new Exception("Lỗi khi cập nhật dữ liệu trong cơ sở dữ liệu", e);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new Exception("Lỗi khi đóng kết nối", ex);
+            }
+        }
+    }
+
+    // Hàm Xóa
+    public static void deleteProduct(int productId) throws Exception {
+        Connection conn = null;
+        Statement stmt = null;
+
+        try {
+            conn = DBConnection.getConnection();
+            stmt = conn.createStatement();
+            String sql = "DELETE FROM products WHERE product_id = " + productId;
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new Exception("Lỗi khi xóa dữ liệu khỏi cơ sở dữ liệu", e);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                throw new Exception("Lỗi khi đóng kết nối", ex);
+            }
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,38 +1,63 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<%@ page import="pxu.edu.vn.product.Product" %>
-<%@ page import="pxu.edu.vn.product.ProductModel" %>
-<%@ page import="java.util.*" %>
-
-
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Danh sách sản phẩm</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 </head>
 <body>
-    <%-- Tạo một đối tượng ProductModel --%>
-    <% ProductModel productModel = new ProductModel(); %>
-    
-    <%-- Lấy danh sách sản phẩm từ ProductModel --%>
-    <% List<Product> products = productModel.getAllProducts(); %>
-    
-    <%-- Hiển thị danh sách sản phẩm --%>
-    <div class="product-list">
-        <%-- Sử dụng vòng lặp để duyệt qua danh sách sản phẩm và hiển thị chúng --%>
-        <% for (Product product : products) { %>
-            <div class="product-item">
-                <h3><%= product.getProductName() %></h3>
-                <p>Giá: <%= product.getPrice() %></p>
-                <%-- Sử dụng cấu trúc if-else để kiểm tra giá giảm giá --%>
-                <% if (product.getDiscountedPrice() > 0) { %>
-                    <p>Giá giảm giá: <%= product.getDiscountedPrice() %></p>
-                <% } %>
-                <img src="<%= product.getProductImg() %>" alt="Product Image">
-                <p><%= product.getProductInfo() %></p>
-            </div>
-        <% } %>
+    <div class="container">
+        <h2>Danh sách sản phẩm</h2>
+
+        <table class="table table-bordered" id="productTable">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tên sản phẩm</th>
+                    <th>ID danh mục</th>
+                    <th>ID thương hiệu</th>
+                    <th>Giá</th>
+                    <th>Giá giảm</th>
+                    <th>Ảnh sản phẩm</th>
+                    <th>Thông tin sản phẩm</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Gọi Ajax để lấy danh sách sản phẩm từ "ProductController.jsp"
+            $.ajax({
+                url: "ProductController.jsp",
+                type: "GET",
+                dataType: "json",
+                success: function(data) {
+                    // Xử lý dữ liệu và hiển thị trong bảng
+                    var productTable = $('#productTable').DataTable({
+                        "data": data,
+                        "columns": [
+                            { "data": "productID" },
+                            { "data": "productName" },
+                            { "data": "categoryID" },
+                            { "data": "brandID" },
+                            { "data": "price" },
+                            { "data": "discountedPrice" },
+                            { "data": "productImg" },
+                            { "data": "productInfo" }
+                        ]
+                    });
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
+                }
+            });
+        });
+    </script>
 </body>
 </html>
